5a91dac86aaf6bb0e7f104ab4c036a8510b859b2
Aidan-Z <aidanzamfir@gmail.com>
2023-07-14 09:54:40 -0300

    working on character function

--- network.py
+++ network.py
@@ -5,8 +5,9 @@ import pandas as pd
 import numpy as np
 import spacy
 
-
 NER = spacy.load("en_core_web_sm")
+#call main df 'iliad_dataframe'        <------------here
+
 class CreateNetwork:
 
     def __init__(self):
@@ -23,13 +24,15 @@ class CreateNetwork:
         for i in self.doc.sents:
             self.entity_list = [j.text for j in i.ents]
             sentence_entity_list.append({'sentence': i, 'character': self.entity_list})
+
         self.sentence_ent_df = pd.DataFrame(sentence_entity_list)
+        # print(self.sentence_ent_df.head(30))
+
 
     def characters(self):
         """From df, get frequency of individual 'character' entities
         & organize in list by frequency order"""
 
         x = self.sentence_ent_df['character'].value_counts()
-        print(x)
         characters = [i for i in x]
-        print(characters) #prints number- need to print order (with name)
+        print(characters)
